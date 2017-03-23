05648a226d92450ec5e8d291fbfe22607c1fc31c
Joe Merten <git@jme.de>
2017-03-23 18:49:30 +0100

    added first small Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,20 @@
+#!/usr/bin/env groovy
+
+
+ansiColor('xterm') {
+
+node() {
+    stage("Init") {
+    	echo "Initializing"
+    }
+
+    stage("Work") {
+    	echo "Do some work"
+    }
+
+    stage("Quit") {
+    	echo "Cleanup"
+    }
+} // node
+
+} // ansiColor
---
+++ Jenkinslib.groovy
@@ -0,0 +1,20 @@
+class AnsiColor {
+    static String black   = "\u001B[30m"  // BLACK
+    static String maroon  = "\u001B[31m"  // RED
+    static String green   = "\u001B[32m"  // GREEN
+    static String olive   = "\u001B[33m"  // BROWN -> Dunkelgelb
+    static String navy    = "\u001B[34m"  // BLUE
+    static String purple  = "\u001B[35m"  // MAGENTA
+    static String teal    = "\u001B[36m"  // CYAN
+    static String silver  = "\u001B[37m"  // LTGRAY -> Dunkelweiss
+    static String gray    = "\u001B[90m"  // GRAY -> Hellschwarz
+    static String red     = "\u001B[91m"  // LTRED
+    static String lime    = "\u001B[92m"  // LTGREEN
+    static String yellow  = "\u001B[93m"  // YELLOW
+    static String blue    = "\u001B[94m"  // LTBLUE
+    static String fuchsia = "\u001B[95m"  // LTMAGENTA
+    static String aqua    = "\u001B[96m"  // LTCYAN
+    static String white   = "\u001B[97m"  // WHITE
+    static String normal  = "\u001B[39m"
+    static String reset   = "\u001B[m"
+}
